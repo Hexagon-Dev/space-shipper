a19638783e1f77235a0441be48f1c48fc55b93b3
Hexagon-Dev <hexagon.dev.ua@gmail.com>
2023-03-11 19:25:21 +0200

    Added randomness posibility for background.

--- scripts/Stars.gd
+++ scripts/Stars.gd
@@ -7,7 +7,13 @@ var player
 
 func _ready():
 	player = $/root/Game/CharacterBody2D
+	
+	var rng = RandomNumberGenerator.new()
+	rng.seed = hash(1111)
+	var displacement = rng.randf_range(0, 6000)
+	print(displacement)
 	material.set_shader_parameter("viewport_size", get_viewport_rect().size)
+	material.set_shader_parameter("displacement", Vector2(displacement, displacement))
 
 func _process(delta):
 	#position = player.position
