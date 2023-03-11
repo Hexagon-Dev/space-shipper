c127196daf31c9bcd0b991fc43c60dad4a2842f6
Hexagon-Dev <hexagon.dev.ua@gmail.com>
2023-03-11 22:20:22 +0200

    Stars for menu.

--- scripts/Stars.gd
+++ scripts/Stars.gd
@@ -1,20 +1,25 @@
 extends ColorRect
 
-var player
-
-#func _process(delta):
-	#position = $/root/Game/CharacterBody2D.position
+var player: BasicShip
+@export var camera: Camera2D
+@export var is_menu: bool = false
+var scene_position = Vector2.ZERO
 
 func _ready():
-	player = $/root/Game/CharacterBody2D
+	if !is_menu:
+		player = $/root/Game/CharacterBody2D
 	
 	var rng = RandomNumberGenerator.new()
-	rng.seed = hash(1111)
+	# TODO: Set seed from save
+	rng.seed = hash(randi_range(0, 1000)) if is_menu else hash(1111)
 	var displacement = rng.randf_range(0, 6000)
 	print(displacement)
 	material.set_shader_parameter("viewport_size", get_viewport_rect().size)
 	material.set_shader_parameter("displacement", Vector2(displacement, displacement))
 
-func _process(delta):
-	#position = player.position
-	material.set_shader_parameter("player_position", player.position)
+func _process(_delta):
+	if is_menu:
+		scene_position += Vector2.UP
+		material.set_shader_parameter("player_position", Vector2(camera.position.x, scene_position.y))
+	else:
+		material.set_shader_parameter("player_position", player.position)
