ef7573930bf7571c64a5d9f39b0d941f7a935cea
Hexagon-Dev <hexagon.dev.ua@gmail.com>
2023-03-11 22:19:06 +0200

    Added main menu, titles and options.

---
+++ scripts/Menu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+const MAIN_MENU = Vector2.ZERO
+const OPTIONS_MENU = Vector2(1152, 0)
+const TITLE_MENU = Vector2(-1152, 0)
+
+var current_menu = "main"
+var destination_menu = "main"
+var destination_position = Vector2.ZERO
+
+@onready var camera: Camera2D = $/root/Menu/Camera2D
+
+func _process(delta):
+	if Input.is_action_just_pressed("escape"):
+		destination_position = MAIN_MENU
+	
+	if camera.position == destination_position:
+		return
+	
+	camera.position = lerp(camera.position, destination_position, 0.05)
+
+func _on_options_pressed():
+	destination_position = OPTIONS_MENU
+
+func _on_options_back_pressed():
+	destination_position = MAIN_MENU
+
+func _on_titles_pressed():
+	destination_position = TITLE_MENU
+
+func _on_titles_back_pressed():
+	destination_position = MAIN_MENU
+
+func _on_exit_pressed():
+	get_tree().quit()
